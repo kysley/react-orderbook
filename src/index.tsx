b00bf0efb6bbb113b8baae044e9ca98eaf1a0f71
Evan Kysley <evan@kysley.com>
2021-09-23 23:47:08 -0400

    add jotai, stitches

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
+import { Provider as JotaiProvider } from 'jotai';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+
+import { App } from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <JotaiProvider>
+      <App />
+    </JotaiProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
