a31685ed4b2fdd7f9df61a0791573e60351137aa
Evan Kysley <evan@kysley.com>
2021-09-25 20:34:05 -0400

    add Button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,15 @@
+import { styled } from '../utils/stitches.conf';
+
+export const Button = styled('button', {
+  color: '$text',
+  backgroundColor: '$purple',
+  display: 'flex',
+  justifyContent: 'center',
+  justifyItems: 'center',
+  padding: '0.5em 1em',
+  fontSize: '.795em',
+  outline: 'none',
+  border: 'none',
+  borderRadius: '5px',
+  cursor: 'pointer',
+});
